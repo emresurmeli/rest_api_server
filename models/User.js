c86a214830053a114e2341feb78e730fd645010c
Emre Surmeli <s.emre.s.8@gmail.com>
2015-05-10 22:49:08 -0700

    small changes to the schema

--- models/User.js
+++ models/User.js
@@ -2,10 +2,16 @@
 
 var mongoose = require('mongoose');
 
+// My schema is based on a dating application, this is the user information
 var userSchema = mongoose.Schema({
   name: String,
   email: String,
+  age: Number,
   gender: {
+  	male: Boolean,
+  	female: Boolean
+  },
+  genderOfInterest: {
   	male: Boolean,
   	female: Boolean
   }
